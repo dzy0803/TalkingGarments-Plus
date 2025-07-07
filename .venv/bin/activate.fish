c65c875a6535d0f74dd83a1f0e95e69846afca42
ziyang <dengziyang84@gmail.com>
2025-07-07 05:15:49 +0100

    Initial clean commit

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ziyangucliot/MAS/.venv
+set -gx VIRTUAL_ENV /home/ziyangucliot/Agents/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
